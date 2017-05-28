add0a93129e3d2357c9b8b397399e72a8519599e
Martin Chobanyan <foxmart36@gmail.com>
2017-05-29 01:15:22 +0400

    Aboutus addition

--- aboutus.php
+++ aboutus.php
@@ -22,13 +22,15 @@ include_once 'admin_panel/scripts/logreader.php';
 <noindex><h1><i class="Y-Glux">Մեր Մասին</i></h1></noindex>
 <p class="Text"><i>
 &nbsp;&nbsp;Կայքը ստեղծվել է աստղագետ-ֆիզիկոսների համար։ Ստեղծման առիթ է հանդիսացել տաղանդաշատ երեխաների գրքերի ծանր որոնումներն տեսնելը։ Կայքի հիմնադիրները և աջակիցներն են՝</i></p><hr>
-<center style="padding-top: 80px; padding-bottom: 80px;"> 
-<!--՜՜՜՜՜՜՜՜Բոլոր տեքստերը գրվել են Ռեդակտոր՝ Մանան Մանուչարյանի կողմից։՜՜՜՜՜՜՜՜-->
+<center style="padding-top: 80px; padding-bottom: 80px;">
 <span style="font-size: 20px; color: #44D5EB">Հեղինակ և ծրագրավորող` 
      <span style="padding-left: 4em; font-size: 30px; color: #ECCCCC">Մարտին Չոբանյան</span>
 </span> <br><br><br>
 <span style="font-size: 20px; color: #44D5EB">Դիզայն-Մաստեր և աջակից` 
      <span style="padding-left: 4em; font-size: 27px; color: #ECCCCC">Արփինե Գրիգորյան</span>
+</span> <br><br><br>
+<span style="font-size: 20px; color: #44D5EB">Ռեդակտոր՝ 
+     <span style="padding-left: 4em; font-size: 27px; color: #ECCCCC">Մանան Մանուչարյան</span>
 </span> <br><br><br>
 </center>
 <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
