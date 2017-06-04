648ffcf9eb63681c1ab15e9e6ec5ae49cd80cfc7
Martin Chobanyan <foxmart36@gmail.com>
2017-06-04 10:32:29 +0400

    Truncater FIX

--- admin_panel/scripts/TRUNCATE-table.php
+++ admin_panel/scripts/TRUNCATE-table.php
@@ -2,5 +2,5 @@
 include_once 'connection.php';
 $query="TRUNCATE TABLE Offers";
 mysqli_query($link, $query);
-header("Location:".$_SERVER['DOCUMENT_ROOT']."/admin_panel/offers.php");
+header("Location:/admin_panel/offers.php");
 ?>
