c8df2db6403d5e0a42905c94e6c167fbc895e55c
Martin Chobanyan <foxmart36@gmail.com>
2018-09-06 20:26:54 +0400

    #Refactoring

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
-$(document).scroll(function(){
-    var a=$("header").height();
-    if($(window).width()>600){a+=23}
-    if($(document).scrollTop()>=a){
+$(document).scroll(function(){ // Функция вызываемая при прокрутке
+    var ContainerHeight = $("header").height(); // Получаем высоту блока перед фиксирующимися объектами
+    if($(window).width() > 600) ContainerHeight += 23; // Добавляем место прокрутки перед фиксацией для девайсов с шириной больше 600px
+    if($(document).scrollTop() >= ContainerHeight){ // Проверяем дистанцию между вершиной страницы и верхней частью браузера
         $("search").addClass("fixed");
         $("ul").addClass("fixed_ul")
     }
